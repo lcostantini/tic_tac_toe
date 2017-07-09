682cf9385f88197e1079a52311d22b30025b503d
lcostantini <costantinileandro1@gmail.com>
2017-07-09 19:21:13 -0300

    Run the worker and supervisor when app start

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -2,6 +2,13 @@ defmodule TicTacToe do
   use Application
 
   def start(_type, _args) do
-    TicTacToe.Supervisor.start_link()
+    import Supervisor.Spec
+
+    children = [
+      supervisor(TicTacToe.GameSupervisor, []),
+      worker(TicTacToe.Registry, [])
+    ]
+
+    Supervisor.start_link(children, strategy: :rest_for_one, name: TicTacToe.Supervisor)
   end
 end
