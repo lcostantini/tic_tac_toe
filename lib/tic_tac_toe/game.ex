098f6e27e707c87cef2930914323ea4603612dcc
lcostantini <costantinileandro1@gmail.com>
2017-07-09 20:12:50 -0300

    Add a new player to the game

--- lib/tic_tac_toe/game.ex
+++ lib/tic_tac_toe/game.ex
@@ -5,7 +5,44 @@ defmodule TicTacToe.Game do
 
   use GenServer
 
+  defstruct board: %TicTacToe.Board{}, player1: nil, player2: nil, waiting: :o
+
   def start_link(name) do
     GenServer.start_link(__MODULE__, :ok, name: name)
+  end
+
+  @doc """
+  Adds a new player to the game.
+  """
+  def join(game, player_pid) do
+    GenServer.call(game, {:join, player_pid})
+  end
+
+  def init(_) do
+    {:ok, %TicTacToe.Game{}}
+  end
+
+  def handle_call({:join, player_pid}, _from, state) do
+    cond do
+      is_nil(state.player1) ->
+        state = %{state | player1: player_pid}
+        {:reply, {:ok, :x}, state}
+      is_nil(state.player2) ->
+        state = %{state | player2: player_pid}
+        state = next_turn(state)
+        {:reply, {:ok, :o}, state}
+      true ->
+        {:reply, :error, state}
+    end
+  end
+
+  defp next_turn(%{waiting: :o, player1: player1, board: board} = state) do
+    send player1, {:play, board}
+    %{state | waiting: :x}
+  end
+
+  defp next_turn(%{waiting: :x, player2: player2, board: board} = state) do
+    send player2, {:play, board}
+    %{state | waiting: :o}
   end
 end
