b3ad675ba898a3e9afa257fd9d8fb8c062c3a8c2
lcostantini <costantinileandro1@gmail.com>
2017-07-09 19:02:24 -0300

    Run Supervisor and Registry when the app start

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -1,2 +1,7 @@
 defmodule TicTacToe do
+  use Application
+
+  def start(_type, _args) do
+    TicTacToe.Supervisor.start_link()
+  end
 end
--- mix.exs
+++ mix.exs
@@ -13,7 +13,8 @@ defmodule TicTacToe.Mixfile do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {TicTacToe, []}
     ]
   end
 
