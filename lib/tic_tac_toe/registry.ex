b11e7a8f19f0ee71f19b68859d1a4c4a121530f5
lcostantini <costantinileandro1@gmail.com>
2017-07-11 14:12:39 -0300

    Find game pid by name or create a new one

--- lib/tic_tac_toe/registry.ex
+++ lib/tic_tac_toe/registry.ex
@@ -9,7 +9,19 @@ defmodule TicTacToe.Registry do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def game_process(name) do
+    case Process.whereis(name) do
+      :undefined -> GenServer.call(__MODULE__, {:game_process, name})
+      pid -> pid
+    end
+  end
+
   def init(:ok) do
     {:ok, %{}}
+  end
+
+  def handle_call({:game_process, name}, _form, state) do
+    game_pid = TicTacToe.GameSupervisor.start_game(name) |> elem(1)
+    {:reply, game_pid, state}
   end
 end
