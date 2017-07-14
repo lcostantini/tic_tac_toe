2855048bf674bbb75369fcdda54f0de0a7284d69
lcostantini <costantinileandro1@gmail.com>
2017-07-14 13:10:34 -0300

    Add game js file to manage view

--- frontend/web/static/js/game.js
+++ frontend/web/static/js/game.js
@@ -1,4 +1,12 @@
 let Game = {
+  game: document.getElementById("game"),
+  waiting: document.getElementById("waiting"),
+  fullGame: document.getElementById("full_game"),
+  xName: document.getElementById("x_name"),
+  oName: document.getElementById("o_name"),
+  xTurn: document.getElementById("x_turn"),
+  oTurn: document.getElementById("o_turn"),
+
   init(socket) {
     socket.connect()
 
@@ -11,7 +19,67 @@ let Game = {
       .receive("ok", resp => {
       })
       .receive("error", reason => {
+        this.show(this.fullGame)
       })
+
+    gameChannel.on("new_player", (resp) => {
+      if (resp.x && resp.o) {
+        this.updateBoard(resp)
+        this.updateStats(resp)
+        this.hide(this.waiting)
+        this.show(this.game)
+        this.show(this.stats)
+      } else {
+        this.show(this.waiting)
+      }
+    })
+
+    gameChannel.on("update_board", (resp) => {
+      this.updateBoard(resp)
+    })
+
+    gameChannel.on("player_left", (resp) => {
+      this.hide(this.stats)
+      this.hide(this.game)
+      this.show(this.waiting)
+    })
+
+    game.addEventListener("click", (e) => {
+      e.preventDefault()
+      let index = e.target.getAttribute("data-index")
+      gameChannel.push("put", {index: index})
+    })
+  },
+
+  show(element) {
+    element.classList.remove("hidden")
+  },
+
+  hide(element) {
+    element.classList.add("hidden")
+  },
+
+  updateBoard(resp) {
+    let data = resp.board.data
+    for (let i = 0; i < data.length; i++) {
+      document.getElementById("index_" + i).innerHTML = data[i]
+    }
+    this.showNext(resp.next)
+  },
+
+  updateStats(game) {
+    this.xName.innerHTML = game.x
+    this.oName.innerHTML = game.o
+  },
+
+  showNext(symbol) {
+    if (symbol == "x") {
+      this.hide(this.oTurn)
+      this.show(this.xTurn)
+    } else {
+      this.hide(this.xTurn)
+      this.show(this.oTurn)
+    }
   }
 }
 
