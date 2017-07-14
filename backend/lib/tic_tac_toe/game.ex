eb3cf1c987b313e42d5d7d1f114920bec2ac0aab
lcostantini <costantinileandro1@gmail.com>
2017-07-14 13:11:04 -0300

    Update backend game code

--- backend/lib/tic_tac_toe/game.ex
+++ backend/lib/tic_tac_toe/game.ex
@@ -5,7 +5,13 @@ defmodule TicTacToe.Game do
 
   use GenServer
 
-  defstruct board: %TicTacToe.Board{}, player1: nil, player2: nil, waiting: :o
+  defstruct(board: %TicTacToe.Board{},
+            x: nil,
+            o: nil,
+            first: :x,
+            next: :x,
+            finished: false
+  )
 
   def start_link(name) do
     GenServer.start_link(__MODULE__, nil, name: via_tuple(name))
@@ -22,8 +28,8 @@ defmodule TicTacToe.Game do
     GenServer.call(game, {:put, value, position})
   end
 
-  def restart_game(game) do
-    GenServer.call(game, :restart_game)
+  def new_round(game) do
+    GenServer.call(game, :new_round)
   end
 
   def whereis(name) do
@@ -38,61 +44,70 @@ defmodule TicTacToe.Game do
     {:ok, %TicTacToe.Game{}}
   end
 
+  def handle_call(:new_round, _from, state) do
+    new_state =
+      %{state | board: %TicTacToe.Board{}, finished: false}
+      |> next_round()
+    {:reply, new_state, new_state}
+  end
+
   def handle_call({:join, player_pid}, _from, state) do
     cond do
-      is_nil(state.player1) ->
-        state = %{state | player1: player_pid}
+      is_nil(state.x) ->
+        state = %{state | x: player_pid}
         {:reply, {:ok, :x, state}, state}
-      is_nil(state.player2) ->
-        state = %{state | player2: player_pid}
-        state = next_turn(state)
+      is_nil(state.o) ->
+        state = %{state | o: player_pid}
         {:reply, {:ok, :o, state}, state}
       true ->
         {:reply, :error, state}
     end
   end
 
-  def handle_call({:put, value, position}, _form, %{waiting: value} = state) do
+  def handle_call({:put, _symbol, _pos}, _from, %{finished: true} = state) do
+    {:reply, :finished, state}
+  end
+
+  def handle_call({:put, value, position}, _form, %{next: value} = state) do
     case TicTacToe.Board.put(state.board, value, position) do
       {:ok, board} ->
+        state = %{state | board: board}
         cond do
           winner = TicTacToe.Board.winner(board) ->
-            notify_winner(state.player1, winner == :x, board)
-            notify_winner(state.player2, winner == :o, board)
+            state = %{state | finished: true}
+            {:reply, {:winner, winner, state}, state}
           TicTacToe.Board.full?(board) ->
-            notify_draw(state.player1, state.player2, board)
+            state = %{state | finished: true}
+            {:reply, {:draw, state}, state}
           true ->
-            state = next_turn(%{state | board: board})
-            {:reply, {:ok, board}, state}
+            state = next_turn(state)
+            {:reply, {:ok, state}, state}
         end
-      {:error, _} -> {:reply, :retry, state}
+      :error -> {:reply, :retry, state}
     end
   end
 
+  def handle_call({:put, _symbol, _position}, _form, state) do
+    {:reply, :cheat, state}
+  end
+
   def handle_call(:restart_game, _from, state) do
-    {:reply, {state.player1, state.player2}, state}
+    {:reply, {state.x, state.o}, state}
   end
 
-  defp notify_winner(player_pid, true, board) do
-    send(player_pid, {:you_won, board})
+  defp next_turn(%{next: :x} = state) do
+    %{state | next: :o}
   end
 
-  defp notify_winner(player_pid, false, board) do
-    send(player_pid, {:you_lost, board})
+  defp next_turn(%{next: :o} = state) do
+    %{state | next: :x}
   end
 
-  defp notify_draw(player1_pid, player2_pid, board) do
-    send(player1_pid, {:draw, board})
-    send(player2_pid, {:draw, board})
+  defp next_round(%{first: :x} = state) do
+    %{state | first: :o, next: :o}
   end
 
-  defp next_turn(%{waiting: :o, player1: player1, board: board} = state) do
-    send player1, {:play, board}
-    %{state | waiting: :x}
-  end
-
-  defp next_turn(%{waiting: :x, player2: player2, board: board} = state) do
-    send player2, {:play, board}
-    %{state | waiting: :o}
+  defp next_round(%{first: :o} = state) do
+    %{state | first: :x, next: :x}
   end
 end
